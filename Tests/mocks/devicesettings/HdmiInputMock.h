6796eb9fe0174c7800b68ae19b04c990b4f2b625
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2022-09-14 14:41:06 -0400

    Update HdmiInputMock.h

--- Tests/mocks/devicesettings/HdmiInputMock.h
+++ Tests/mocks/devicesettings/HdmiInputMock.h
@@ -11,4 +11,17 @@ public:
     MOCK_METHOD(uint8_t, getNumberOfInputs, (), (const, override));
     MOCK_METHOD(bool, isPortConnected, (int8_t Port), (const, override));
     MOCK_METHOD(std::string, getCurrentVideoMode, (), (const, override));
+
+    MOCK_METHOD(vector<uint8_t>, getEDIDBytesInfo, (int8_t iPort, vector<uint8_t> edidVec2), (const, override));
+    MOCK_METHOD(vector<uint8_t>, getHDMISPDInfo, (int8_t iPort, vector<uint8_t> edidVec2), (const, override));
+    MOCK_METHOD(int8_t, setEdidVersion, (int8_t iPort, int8_t iEdidVer), (const, override));
+    MOCK_METHOD(int8_t, getEdidVersion, (int8_t iPort, int8_t edidVersion), (const, override));
+
+    MOCK_METHOD(int8_t, selectPort, (int8_t iPort), (const, override));
+    MOCK_METHOD(int8_t, scaleVideo, (int8_t x, int8_t y, int8_t width, int8_t height), (const, override));
+
+    MOCK_METHOD(vector<string>, getSupportedGameFeatures, (vector<string> supportedFeatures), (const, override));
+
+    MOCK_METHOD(bool, getHdmiALLMStatus, (int8_t iPort, bool allm), (const, override));
+
 };
