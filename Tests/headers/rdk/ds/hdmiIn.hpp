caf51e0398c6bd75b539e0928c9afae3618863a1
hgfell683 <107510770+hgfell683@users.noreply.github.com>
2022-09-16 13:19:17 -0400

    Update hdmiIn.hpp

--- Tests/headers/rdk/ds/hdmiIn.hpp
+++ Tests/headers/rdk/ds/hdmiIn.hpp
@@ -12,18 +12,18 @@ public:
     virtual bool isPortConnected(int8_t Port) const = 0;
     virtual std::string getCurrentVideoMode() const = {'u','n','k','n','o','w','n' };
 
-    virtual vector<uint8_t> getEDIDBytesInfo(int8_t iPort, vector<uint8_t> edidVec2) const = {'u','n','k','n','o','w','n' };
-    virtual vector<uint8_t> getHDMISPDInfo(int8_t iPort, vector<uint8_t> edidVec2) const = {'u','n','k','n','o','w','n' };
+    virtual std::string getEDIDBytesInfo(int8_t iPort, vector<uint8_t> edidVec2) const = {'u','n','k','n','o','w','n' };
+    virtual std::string getHDMISPDInfo(int8_t iPort, vector<uint8_t> edidVec2) const = {'u','n','k','n','o','w','n' };
     virtual int8_t setEdidVersion(int8_t iPort, int8_t iEdidVer) const = 0;
     virtual int8_t setEdidVersion(int8_t iPort, int8_t edidVersion) const = 0;
 
     virtual int8_t selectPort(int8_t iPort) const = 0;
-    virtual int8_t setVideoRectangle(int8_t x, int8_t y, int8_t width, int8_t height) const = 0;
 
-    virtual vector<uint8_t> getEDIDBytesInfo(vector<string> supportedFeatures) const = {'u','n','k','n','o','w','n' };
+    virtual int8_t scaleVideo(int8_t x, int8_t y, int8_t width, int8_t height) const = 0;
+    virtual std::string getSupportedGameFeatures(vector<string> supportedFeatures) const = {'u','n','k','n','o','w','n' };
 
 
-    bool getHdmiALLMStatus(int8_t iPort, bool allm) const = 0;
+    virtual bool getHdmiALLMStatus(int8_t iPort, bool allm) const = 0;
 
 
 };
@@ -51,11 +51,11 @@ public:
         return impl->getCurrentVideoMode();
     }
 
-    std::string vector<uint8_t> getEDIDBytesInfo(int8_t iPort, vector<uint8_t> edidVec2) const
+    std::string getEDIDBytesInfo(int8_t iPort, vector<uint8_t> edidVec2) const
     {
         return impl->getEDIDBytesInfo(int8_t iPort, vector<uint8_t> edidVec2);
     }
-    std::string vector<uint8_t> getHDMISPDInfo(int8_t iPort, vector<uint8_t> edidVec2) const
+    std::string getHDMISPDInfo(int8_t iPort, vector<uint8_t> edidVec2) const
     {
         return impl->getHDMISPDInfo(int8_t iPort, vector<uint8_t> edidVec2);
     }
@@ -73,19 +73,17 @@ public:
         return impl->selectPort(int8_t iPort);
     }
 
-    int8_t setVideoRectangle(int8_t x, int8_t y, int8_t width, int8_t height) const
+    int8_t scaleVideo(int8_t x, int8_t y, int8_t width, int8_t height) const
     {
-        return impl->setVideoRectangle(int8_t x, int8_t y, int8_t width, int8_t height);
-    }
-    vector<uint8_t> getEDIDBytesInfo(vector<string> supportedFeatures) const
-    {
-        return impl->getEDIDBytesInfo(vector<string> supportedFeatures);
+        return impl->scaleVideo(int8_t x, int8_t y, int8_t width, int8_t height);
     }
 
-    vector<uint8_t> getEDIDBytesInfo(vector<string> supportedFeatures) const
+    
+    std::string getSupportedGameFeatures(vector<string> supportedFeatures) const
     {
-        return impl->getEDIDBytesInfo(vector<string> supportedFeatures);
+        return impl->getSupportedGameFeatures(vector<string> supportedFeatures);
     }
+
 
     bool getHdmiALLMStatus(int8_t iPort, bool allm) const
     {
@@ -94,3 +92,4 @@ public:
 };
 
 }
+
